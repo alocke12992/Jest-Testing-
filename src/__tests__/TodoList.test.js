c8ca83e6587300aa86af333f9514250db6c04281
Andrew Locke <alocke12992@gmail.com>
2018-04-09 11:25:10 -0600

    Adds first tests

---
+++ src/__tests__/TodoList.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import TodoList from '../TodoList';
+
+describe(<TodoList />, () => {
+
+  describe('render', () => {
+    let component;
+    beforeEach(() => {
+      component = shallow(<TodoList />)
+    });
+
+    it('matches snapshot', () => {
+      const tree = toJson(component)
+      expect(tree).toMatchSnapshot();
+    })
+  })
+
+  describe('functionality', () => {
+    let component;
+    beforeEach(() => {
+      component = shallow(<TodoList />)
+    });
+
+    it('updates state on change', () => {
+      const expected = 'Hello World';
+      //find the input
+      let input = component.find('input')
+
+      //focus the input
+      input.simulate('focus');
+      input.simulate('change',
+        {
+          target:
+            {name: 'name', value: expected}
+        }
+      )
+      const actual = component.state('name')
+      expect(actual).toEqual(expected)
+    })
+
+
+  })
+
+})
